e3146d84bfc348eae6056ca5dd7f3f6b0866f371
Kerby <kn1578@nyu.edu>
2020-09-14 22:19:31 +0800

    Test (Lesson 2)

---
+++ Assets/movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class movement : MonoBehaviour {
+    // Start is called before the first frame update
+    float speed = 5; 
+
+        void Start() {
+        ControllPlayer();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+
+
+    }
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    public Sprite newSprite2;
+
+    void Update() {
+        if(Time.time > 5 && Time.time<30) {
+            spriteRenderer.sprite = newSprite;
+        }
+
+        if (Time.time > 15)
+        {
+            spriteRenderer.sprite = newSprite2;
+        }
+
+        //WASD INPUT 
+        if (Input.GetKey(KeyCode.W)){
+            transform.Translate(0, speed * Time.deltaTime, 0);
+
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+
+        }
+        
+        if(Input.GetKey(KeyCode.A)){
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        }
+    }
+
+    void ControllPlayer()
+    {
+       
+
+    }
+}
---
+++ Assets/spacebargame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spacebargame : MonoBehaviour
+{
+
+    public int myScore = 0; 
+    public int scoreVictory = 20; 
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        Debug.Log("DEBUG");
+        print ("this is print? yikes");
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            
+            myScore++; 
+            print ("spacebar pressed");
+
+        }
+    }
+}
